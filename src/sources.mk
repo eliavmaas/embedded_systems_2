739fc8d2c764df1d925ccbc7e318aa67a4523b11
eliavmaas <eliavmaas@gmail.com>
2021-09-26 12:04:03 -0700

    figuring out includes formatting

--- src/sources.mk
+++ src/sources.mk
@@ -10,11 +10,19 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
+ifeq ($(PLATFORM),MSP432)
 SOURCES = main.c \
 		memory.c \
-		system src/startup_msp432p401r_gcc.c\
+		interrupts_msp432p401r_gcc.c\
+		startup_msp432p401r_gcc.c\
 		system_msp432p401r.c
+else
+SOURCES = main.c \
+		memory.c
+endif
+
+
 
 # Add your include paths to this variable
-INCLUDES = -l
+INCLUDES = /../
 
